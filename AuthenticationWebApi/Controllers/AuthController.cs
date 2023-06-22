6d09ec6a19590f8954aafbc9f4b662d311014af2
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-06-22 07:35:09 +0530

    Add project files.

---
+++ AuthenticationWebApi/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using AuthenticationWebApi.Models.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public async Task<ActionResult<User>> RegisterUser()
+        {
+
+        }
+    }
+}
---
+++ AuthenticationWebApi/Models/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationWebApi.Models.Dtos
+{
+    public class UserDto
+    {
+        public string  Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
